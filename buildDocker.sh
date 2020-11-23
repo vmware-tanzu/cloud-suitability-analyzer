850677b53b5bb88b74aa3e8eb08b17ae26b4b464
Steve Woods <swoods@vmware.com>
2020-11-23 13:07:55 -0600

    Title: Feature - Edit build script to run in action
    Why: Had to remove -it
    
    What: Build script
    
    How: (list of changes):
    Signed-off-by: Steve Woods <swoods@vmware.com>

--- buildDocker.sh
+++ buildDocker.sh
@@ -3,13 +3,13 @@ echo "~~~> Mounting docker image to build web ui"
 
 echo "---> Ruquested OS builds..: $1"
 echo "---> Version..............: $2"
-docker run -it -e OS=$1 -e VERSION=$2 \
+docker run -e OS=$1 -e VERSION=$2 \
    -v ${PWD}:/cloud-suitability-analyzer \
    neur0manc3r/node-go-cross-build \
    ./buildWebUI.sh
 
 echo "~~~> Mounting docker image to build go executable and bind in web"   
-docker run -it -e OS=$1 -e VERSION=$2 \
+docker run -e OS=$1 -e VERSION=$2 \
    -v ${PWD}/:/cloud-suitability-analyzer \
    neur0manc3r/node-go-cross-build \
    ./build-CSA-Bind-UI.sh
