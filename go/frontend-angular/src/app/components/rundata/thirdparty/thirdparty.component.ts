39f12efde35c6c107e9c191f6300782e59c34356
Rohit Bajaj <bajajro@bajajro-a02.vmware.com>
2021-06-03 18:57:40 -0400

    adding third party libs screen

--- go/frontend-angular/src/app/components/rundata/thirdparty/thirdparty.component.ts
+++ go/frontend-angular/src/app/components/rundata/thirdparty/thirdparty.component.ts
@@ -1,22 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
+import {ActivatedRoute, ParamMap, Router} from "@angular/router";
 import {RundataService} from "../../../services/rundata.service";
+import {ThirdPartyLibsUsage} from "../../../model/thirdpartylibsusage";
 
 @Component({
   selector: 'thirdparty',
   templateUrl: './thirdparty.component.html',
-  styleUrls: ['./thirdparty.component.css']
+  styleUrls: ['./thirdparty.component.css','../rundatasummary/rundatasummary.component.css']
 })
 export class ThirdPartyComponent implements OnInit {
 
   public searchCrit: any = '';
-  public fileName: string = 'thirdparty-libs-usage.xlsx';
+  public fileName: string = 'annotations.xlsx';
+  thirdPartyLibs :ThirdPartyLibsUsage[]=[];
 
-  constructor(private router: Router, private rundataService: RundataService) {
+  constructor(private router: Router, private route: ActivatedRoute, private rundataService: RundataService) {
 
   }
 
+
   ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.resetPage();
+      const runId = Number(params.get('id'));
+      console.log("runid is : "+runId);
+      this.fetchApiUsageDetailed(runId);
+    });
+  }
+
+  resetPage(): void {
+    this.thirdPartyLibs=[];
+  }
+
+  fetchApiUsageDetailed(runId: number): void{
+    this.rundataService.getThirdPartyData(runId).subscribe(thirdPartyLibsReturned => {
+      console.log(thirdPartyLibsReturned);
+      this.thirdPartyLibs = thirdPartyLibsReturned;
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
