4c1f26819f81c6f45e5314ca179540b14bccf097
Sylvain Carbonell <scarbonell@scarbonellMML85.vmware.com>
2023-05-15 12:55:06 -0400

    Add more execuatbles for other platforms

--- csa-test/build.sh
+++ csa-test/build.sh
@@ -8,7 +8,11 @@ cp -r -f src/test/test-samples/ test-dist/test-samples
 
 cd src/test/
 go test -c -o unit-test
+GOOS=windows CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ GOARCH=amd64 go test -c -o unit-test-w
+GOOS=linux GOARCH=amd64 go test -c -o unit-test-l
 cd ../../
 mv src/test/unit-test test-dist/unit-test
+mv src/test/unit-test-l test-dist/unit-test-l
+mv src/test/unit-test-w test-dist/unit-test-w
 chmod +x test-dist/unit-test
 
